b5343d67899c4f6db360d48f611ab2c5ab4659aa
waqassafdar221 <waqassafdar221@gmail.com>
2025-03-22 00:27:46 +0500

    deleting_votes

--- posts/serializers.py
+++ posts/serializers.py
@@ -4,9 +4,12 @@ from .models import Post, Vote
 class PostSerializer(serializers.ModelSerializer):
     poster = serializers.ReadOnlyField(source='poster.username')
     poster_id = serializers.ReadOnlyField(source='poster.id')
+    votes = serializers.SerializerMethodField()
     class Meta:
         model = Post
         fields = ['id', 'title', 'url', 'poster','poster_id', 'created_at']
+    def get_votes(self, post):
+        return Vote.objects.filter(post=post).count()
 
 
 class VoteSerializer(serializers.ModelSerializer):
